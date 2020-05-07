18ad34e556ac6622ff9d6676d689c00db513b4a7
Erica Liu <eliu925@gmail.com>
2020-05-07 12:22:08 -0400

    added test specs for wrap

--- wrap.spec.js
+++ wrap.spec.js
@@ -10,7 +10,6 @@ describe('wrap', () => {
   })
   it('Returns a string with line breaks at right places to make sure line is no longer than column number', () => {
     expect(wrap("Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.", 20)).to.equal('Lorem ipsum dolor\nsit eu amet, elit na\nmagna sem amet nulla\nvel purus ac ligula.')
-
     }
     )
   });
